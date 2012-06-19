7c53bfec8f878afcc5419cb09769d6a36de66c82
Jakub Zygmunt <jakub.zygmunt@cloudreach.co.uk>
2012-06-19 17:56:57 +0100

    new version of daily report splunk, using splunky instead of phantomjs

---
+++ splunky.py
@@ -0,0 +1,124 @@
+import ConfigParser
+import re
+import csv
+import splunklib
+from splunklib import client, results, testlib
+
+__author__ = 'jakub.zygmunt'
+class Splunky(object):
+    def __init__(self, username=None, password=None, host=None, port=None, config_file=None, app=None):
+        self.service = None
+        if config_file:
+            config = self.__load_from_config(config_file=config_file)
+        else:
+            config = { 'username': username, 'password': password, 'host': host, 'port': port}
+
+        if app is not None:
+            config['app'] = app
+
+        try:
+            self.service = client.connect(**config)
+        except Exception, e:
+            raise SplunkyCannotConnect(message=e)
+
+
+
+    def __load_from_config(self, config_file):
+        parser = ConfigParser.SafeConfigParser()
+        try:
+            parser.read(config_file)
+            config_dict = {x[0]:x[1] for x in parser.items('splunk')}
+        except (TypeError, ConfigParser.NoSectionError, ConfigParser.MissingSectionHeaderError):
+            config_dict = dict()
+        return config_dict
+
+    def search(self, search=None, saved_search=None):
+        jobs = self.service.jobs
+        job = None
+        results = []
+        try:
+            if search is not None:
+                job = jobs.create(search, exec_mode="blocking", max_count=50000)
+            if saved_search is not None:
+                job = saved_search.dispatch()
+            if job:
+                results = self.get_results(job)
+        except splunklib.binding.HTTPError:
+            # wrong search ?
+            pass
+        return results
+
+    def get_results(self, job):
+        testlib.wait(job, lambda job: bool(int(job['isDone'])))
+        reader = splunklib.results.ResultsReader(job.results(count=0))
+        output = []
+        for kind, result in reader:
+            if 'preview' not in result.keys():
+                if '$offset' in result.keys():
+                    del result['$offset']
+                output.append(result)
+        return output
+
+    def output_to_csv(self, search, csv_file):
+        results = self.search(search=search)
+        header = self.get_header(search=search)
+        self.__list_dict_to_file(results=results, header=header, outfile=csv_file)
+
+    def __list_dict_to_file(self, results, header, outfile):
+        csv_writer = csv.writer(open(outfile, 'w'), delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(header)
+        for dict_row in results:
+            csv_row = []
+            for key in header:
+                try:
+                    csv_row.append(dict_row[key])
+                except KeyError:
+                    csv_row.append('')
+            csv_writer.writerow(csv_row)
+
+    @staticmethod
+    def get_table_fields(search):
+        header = []
+        match = re.search(r'.*\| ?table ([a-zA-Z_0-9, ]+)', search)
+        if match:
+            str = match.group(1)
+            fields = re.findall('([a-zA-Z_0-9]+)+', str)
+            for f in fields:
+                header.append(f)
+        return header
+
+    @staticmethod
+    def get_renames(search):
+        renames = {}
+        match = re.search(r'.*\| ?rename ([a-zA-Z0-9_, "()]+)', search)
+        if match:
+            str = match.group(1)
+            fields = re.findall('([a-zA-Z0-9_]+) as (?:"([^"]+)"|([a-zA-Z0-9_]+))', str)
+            for f in fields:
+                key = f[0]
+                value = f[1] if f[1] != '' else f[2]
+                renames[key] = value
+        return renames
+
+
+    @staticmethod
+    def get_header(search):
+        header = Splunky.get_table_fields(search)
+        if len(header) > 0:
+
+            renames = Splunky.get_renames(search)
+            keys_renames = renames.keys()
+            if len(keys_renames) > 0:
+                new_header = []
+                for col in header:
+                    new_column = renames[col] if col in keys_renames else col
+                    new_header.append(new_column)
+                header = new_header
+
+        return header
+
+
+
+class SplunkyCannotConnect(Exception):
+    def __init__(self, message):
+        self.message=message
--- test_dailyreport.py
+++ test_dailyreport.py
@@ -5,7 +5,6 @@ from dailyreport import DailyReport
 __author__ = 'jakub.zygmunt'
 
 class DailyReportTest(unittest.TestCase):
-
     def load_config(self):
         config_file = 'config/confidential.conf'
         parser = ConfigParser.SafeConfigParser()
@@ -15,8 +14,8 @@ class DailyReportTest(unittest.TestCase):
                 for x in parser.items(section):
                     setattr(self, x[0], x[1])
         except ConfigParser.NoSectionError:
-            self.url = ''
-            self.url_static = ''
+            self.host = ''
+            self.port = ''
             self.splunk_username = ''
             self.splunk_password = ''
             self.auth_username = ''
@@ -25,6 +24,7 @@ class DailyReportTest(unittest.TestCase):
             self.use_ssl = 0
             self.use_tls = 0
             self['from'] = ''
+            self.index_name = None
 
 
     def read_file(self, filename):
@@ -44,28 +44,14 @@ class DailyReportTest(unittest.TestCase):
     def test_should_return_app_names_with_dailyreport_config(self):
         splunk_home = 'test_splunk'
         expected_item = ['%s/etc/apps/app1/local/dailyreport.conf' % splunk_home,
-                         '%s/etc/apps/app3/local/dailyreport.conf' % splunk_home ]
+                         '%s/etc/apps/app3/local/dailyreport.conf' % splunk_home]
         dr = DailyReport(splunk_home=splunk_home)
         apps_with_config = dr.get_apps()
         self.assertEquals(expected_item, apps_with_config)
 
-    def test_should_return_none_when_base_url_is_not_set(self):
-        expected_url = None
-        dr = DailyReport()
-        output_url = dr.get_url_to_homepage('app_folder')
-        self.assertEquals(expected_url, output_url)
-
-    def test_should_return_link_to_homepage(self):
-        expected_url = 'http://127.0.0.1:8000/en-US/app/app1/awsManagedServicesHomepage'
-        splunk_home = 'test_splunk'
-        input_string = '%s/etc/apps/app1/local/dailyreport.conf' % splunk_home
-
-        dr = DailyReport(splunk_home=splunk_home, base_url='http://127.0.0.1:8000/')
-        output_url = dr.get_url_to_homepage(input_string)
-        self.assertEquals(expected_url, output_url)
 
     def test_should_return_empty_list_on_no_config_file(self):
-        expected_email_addresses = [ ]
+        expected_email_addresses = []
         splunk_home = 'test_splunk'
         input_string = '%s/etc/apps/app2/local/dailyreport.conf' % splunk_home
 
@@ -74,7 +60,7 @@ class DailyReportTest(unittest.TestCase):
         self.assertEquals(expected_email_addresses, emailAddresses)
 
     def test_should_return_empty_list_on_invalid_config_file(self):
-        expected_email_addresses = [ ]
+        expected_email_addresses = []
         splunk_home = 'test_splunk'
         input_string = '%s/etc/apps/app3/local/dailyreport.conf' % splunk_home
 
@@ -83,7 +69,7 @@ class DailyReportTest(unittest.TestCase):
         self.assertEquals(expected_email_addresses, emailAddresses)
 
     def test_should_return_two_email_addresses(self):
-        expected_email_addresses = [ 'test1@cloudreach.co.uk', 'test2+something@cloudreach.co.uk']
+        expected_email_addresses = ['test1@cloudreach.co.uk', 'test2+something@cloudreach.co.uk']
         splunk_home = 'test_splunk'
         input_string = '%s/etc/apps/app1/local/dailyreport.conf' % splunk_home
 
@@ -100,27 +86,49 @@ class DailyReportTest(unittest.TestCase):
         nothing, output_title = dr.get_email_app_config(input_string)
         self.assertEquals(expected_title, output_title)
 
+    def test_should_return_empty_index_name_on_no_config_file(self):
+        expected_index_name = None
+        splunk_home = 'test_splunk'
+        input_string = '%s/etc/apps/app2/local/dailyreport.conf' % splunk_home
 
-    def test_should_return_no_content_on_invalid_url(self):
-        expected_content = ''
-        invalid_url = 'http://127.0.0.1:8000/totally_invalid_url'
         dr = DailyReport()
-        output = dr.get_report(url=invalid_url)
-        self.assertEquals(expected_content, output)
+        index_name = dr.get_index_app_config(input_string)
+        self.assertEquals(expected_index_name, index_name)
+
+    def test_should_return_empty_index_name_on_invalid_config_file(self):
+        expected_index_name = None
+        splunk_home = 'test_splunk'
+        input_string = '%s/etc/apps/app3/local/dailyreport.conf' % splunk_home
+
+        dr = DailyReport()
+        index_name = dr.get_index_app_config(input_string)
+        self.assertEquals(expected_index_name, index_name)
+
+    def test_should_return_index_name_on_valid_config_file(self):
+        expected_index_name = 'splunk-index'
+        splunk_home = 'test_splunk'
+        input_string = '%s/etc/apps/app1/local/dailyreport.conf' % splunk_home
+
+        dr = DailyReport()
+        index_name = dr.get_index_app_config(input_string)
+        self.assertEquals(expected_index_name, index_name)
 
     def test_should_return_no_content_on_invalid_credentials(self):
         self.load_config()
         expected_content = ''
-        dr = DailyReport()
-        output = dr.get_report(url=self.url, username='invalid', password='nopassword' )
+        dr = DailyReport(host=self.host, port=self.port, username='invalid', password='nopassword')
+        output = dr.get_report(index_name='lalala')
         self.assertEquals(expected_content, output)
+
 
     def test_should_return_content_on_valid_connection_data(self):
         # update connection.conf
         self.load_config()
-        dr = DailyReport()
-        output = dr.get_report(url=self.url, url_static=self.url_static, username=self.splunk_username, password=self.splunk_password )
+        dr = DailyReport(host=self.host, port=self.port, username=self.splunk_username,
+            password=self.splunk_password)
+        output = dr.get_report(index_name=self.index_name)
         self.assertTrue(len(output) > 0)
+
 
     def test_should_return_empty_mailer_settings_on_invalid_splunk_home(self):
         expected_config = {}
@@ -128,45 +136,42 @@ class DailyReportTest(unittest.TestCase):
         output_config = dr.mailer_config
         self.assertEqual(expected_config, output_config)
 
+
     def test_should_return_default_mailer_settings_with_valid_splunk_home(self):
-        expected_config = {'auth_username':'default_user',
-                          'auth_password':'default_password',
-                          'mailserver':'default.mailserver:123',
-                          'use_ssl':'0',
-                          'use_tls':'0',
-                          'from':'default_from@localhost'
-                          }
+        expected_config = {'auth_username': 'default_user',
+                           'auth_password': 'default_password',
+                           'mailserver': 'default.mailserver:123',
+                           'use_ssl': '0',
+                           'use_tls': '0',
+                           'from': 'default_from@localhost'
+        }
         dr = DailyReport(splunk_home='test_splunk')
         output_config = dr.mailer_config
         self.assertEqual(expected_config, output_config)
 
+
     def test_should_return_updated_mailer_settings_with_valid_splunk_home(self):
-        expected_config = {'auth_username':'new_user',
-                           'auth_password':'new_password',
-                           'mailserver':'new.mailserver:567',
-                           'use_ssl':'0',
-                           'use_tls':'1',
-                           'from':'default_from@localhost'
+        expected_config = {'auth_username': 'new_user',
+                           'auth_password': 'new_password',
+                           'mailserver': 'new.mailserver:567',
+                           'use_ssl': '0',
+                           'use_tls': '1',
+                           'from': 'default_from@localhost'
         }
         mailer_config = 'test_splunk/etc/apps/app4/local/mailer.conf'
-        dr = DailyReport(splunk_home='test_splunk', config= mailer_config)
+        dr = DailyReport(splunk_home='test_splunk', config=mailer_config)
         output_config = dr.mailer_config
         self.assertEqual(expected_config, output_config)
 
-    def test_should_update_base_url(self):
-        expected_base_url = 'http://newbase.url'
-        mailer_config = 'test_splunk/etc/apps/app4/local/mailer.conf'
-        dr = DailyReport(splunk_home='test_splunk', config= mailer_config)
-        self.assertEqual(expected_base_url, dr.base_url)
 
     def test_should_send_email_to_private_account(self):
         self.load_config()
         dr = DailyReport()
-        mail_fields = ['auth_username', 'auth_password','mailserver','use_ssl','use_tls','from']
+        mail_fields = ['auth_username', 'auth_password', 'mailserver', 'use_ssl', 'use_tls', 'from']
         for x in mail_fields:
             dr.mailer_config[x] = getattr(self, x)
 
-        bodyHTML =  'This is <b>HTML</b> email'
+        bodyHTML = 'This is <b>HTML</b> email'
         dr.send_email(to=self.test_to, html_body=bodyHTML)
         # check mailbox
         self.assertTrue(True)
